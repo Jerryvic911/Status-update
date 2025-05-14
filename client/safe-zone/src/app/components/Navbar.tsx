51970385bf08b05c66242f90034f72ec15cf1053
jeremiahv <jeremiahv791@gmail.com>
2025-05-14 10:34:57 +0100

    creation of profile page and profile icon added to navbar

--- client/safe-zone/src/app/components/Navbar.tsx
+++ client/safe-zone/src/app/components/Navbar.tsx
@@ -1,18 +1,28 @@
+import Link from "next/link"
+
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-white/30 backdrop-blur-lg border-b border-white/40 shadow-lg">
-    <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold text-white">Safe-zone</h1>
-      <ul className="flex gap-6 text-white">
-        <li className="hover:text-blue-300 cursor-pointer">Home</li>
-        <li className="hover:text-blue-300 cursor-pointer">About</li>
-        <li className="hover:text-blue-300 cursor-pointer">Contact</li>
-        <li className="hover:text-blue-300 cursor-pointer">login</li>
-        <li className="hover:text-blue-300 cursor-pointer">Sign-up</li>
-      </ul>
-    </div>
-  </nav>
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        <h1 className="text-xl font-bold text-white">Safe-zone</h1>
+        <ul className="flex gap-6 text-white">
+          <li className="hover:text-blue-300 cursor-pointer">Home</li>
+          <li className="hover:text-blue-300 cursor-pointer">About</li>
+          <li className="hover:text-blue-300 cursor-pointer">Contact</li>
+          <li className="hover:text-blue-300 cursor-pointer">login</li>
+          <li className="hover:text-blue-300 cursor-pointer">Sign-up</li>
+          <Link href="/profile">
+          
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-7 hover:cursor-pointer">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          </svg>
+          </li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
---
+++ client/safe-zone/src/app/profile/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+import { IoChevronBackCircle } from "react-icons/io5";
+
+const Profile = () => {
+  return (
+    <div>
+      <div> 
+        <Link href="/">
+         <IoChevronBackCircle className='size-7' />
+        </Link>
+      </div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Profile
